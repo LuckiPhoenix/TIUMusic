8f7cb4b7e0e30dec9b12f6dd9470112946cf2486
kohrabi <63339885+kohrabi@users.noreply.github.com>
2024-11-29 19:55:46 +0700

    test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
     kotlin("plugin.serialization") version "2.0.20"
     id("com.google.dagger.hilt.android")
     id("kotlin-kapt")
-    id("org.jetbrains.kotlin.plugin.serialization")
 }
 
 android {
