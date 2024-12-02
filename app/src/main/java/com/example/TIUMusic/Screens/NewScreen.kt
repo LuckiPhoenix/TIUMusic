8b6a648163f48ac54b866a2eea52d64c8db805ef
kohrabi <63339885+kohrabi@users.noreply.github.com>
2024-12-02 21:44:59 +0700

    add: navigation for NewScreen

--- app/src/main/java/com/example/TIUMusic/Screens/NewScreen.kt
+++ app/src/main/java/com/example/TIUMusic/Screens/NewScreen.kt
@@ -27,7 +27,7 @@ import kotlinx.coroutines.launch
 fun NewScreen(
     navController: NavController,
     onTabSelected: (Int) -> Unit,
-    onPlaylistClick: (MusicItem) -> Unit,
+    onItemClick: (MusicItem) -> Unit,
     ytmusicViewModel: YtmusicViewModel,
     playerViewModel: PlayerViewModel
 ) {
@@ -72,7 +72,9 @@ fun NewScreen(
                     items = newReleaseMusicItems,
                     itemWidth = 300.dp,
                     sectionHeight = 300.dp,
-                    onItemClick = { }
+                    onItemClick = { musicItem ->
+                        onItemClick(musicItem)
+                    }
                 )
             }
 
@@ -85,8 +87,7 @@ fun NewScreen(
                     itemWidth = 300.dp,
                     sectionHeight = 260.dp,
                     onItemClick = { musicItem ->
-                        playerViewModel.resetPlaylist();
-                        playerViewModel.playSong(musicItem, context)
+                        onItemClick(musicItem);
                     }
                 )
             }
@@ -100,9 +101,7 @@ fun NewScreen(
                     itemWidth = 150.dp,
                     sectionHeight = 220.dp,
                     onItemClick = { musicItem ->
-                        Log.d("LogNav", "TYPE = 0")
-                        playerViewModel.resetPlaylist();
-                        playerViewModel.playSong(musicItem, context)
+                        onItemClick(musicItem);
                     }
                 )
             }
@@ -120,7 +119,9 @@ fun NewScreen(
                     items = newAlbumReleases,
                     itemWidth = 150.dp,
                     sectionHeight = 220.dp,
-                    onItemClick = { }
+                    onItemClick = { it ->
+                        onItemClick(it);
+                    }
                 )
             }
 
@@ -138,9 +139,7 @@ fun NewScreen(
                     itemWidth = 150.dp,
                     sectionHeight = 220.dp,
                     onItemClick = { musicItem ->
-                        Log.d("LogNav", "TYPE = 0")
-                        playerViewModel.resetPlaylist();
-                        playerViewModel.playSong(musicItem, context)
+                        onItemClick(musicItem);
                     }
                 )
             }
